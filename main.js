63a7bd2a592d6cd22a2966d8700fc83118c5d4b4
Elodie <elodie.anglade@gmail.com>
2022-06-06 16:55:38 +0200

    updated

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import './style.css'
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+  GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {
+  RGBELoader
+} from 'three/examples/jsm/loaders/RGBELoader.js';
 
 let camera, scene, renderer;
 
@@ -12,64 +18,80 @@ render();
 
 function init() {
 
-  const container = document.createElement( 'div' );
-  document.body.appendChild( container );
+  const container = document.createElement('div');
+  document.body.appendChild(container);
 
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-  camera.position.set( - 1.8, 0.6, 2.7 );
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+  camera.position.set(-1.8, 0.6, 2.7);
+
 
   scene = new THREE.Scene();
 
+  
+  const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  scene.add( directionalLight );
+
   new RGBELoader()
-    .setPath( 'assets/' )
-    .load( 'studio_small_09_1k.hdr', function ( texture ) {
+    .setPath('assets/')
+    .load('elodie.hdr', function (texture) {
 
       texture.mapping = THREE.EquirectangularReflectionMapping;
 
-      scene.background = texture;
+      // A DECOMMENTER POUR DEBUG HDRI
+      //scene.background = texture;
       scene.environment = texture;
 
       render();
 
-      // model
 
-      const loader = new GLTFLoader().setPath( 'assets/models/' );
-      loader.load( 'product.gltf', function ( gltf ) {
+      const loader = new GLTFLoader().setPath('assets/models/');
+      loader.load('product_v3.gltf', function (gltf) {
+        console.log(gltf.scene.children[0].children)
         const glass = gltf.scene.children[0].children.find((mesh) => mesh.name === "product-verre");
-        console.log( glass)
+        console.log(glass)
+
+        let color = new THREE.Color( 0xb8b8b8 );
 
         const glassMaterial = new THREE.MeshPhysicalMaterial({
-          transmission: 1,
+          color: color,
+          transmission: 0.5,
           thickness: 1,
           roughness: 0.5,
+
         });
 
         glass.material = glassMaterial;
 
-        scene.add( gltf.scene );
+        scene.add(gltf.scene);
 
         render();
 
-      } );
+      });
 
-    } );
+    });
 
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+  });
+  renderer.setClearColor(0x000000, 0); // the default
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.toneMapping = THREE.ACESFilmicToneMapping;
   renderer.toneMappingExposure = 1;
   renderer.outputEncoding = THREE.sRGBEncoding;
-  container.appendChild( renderer.domElement );
 
-  const controls = new OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', render ); // use if there is no animation loop
+
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render); // use if there is no animation loop
   controls.minDistance = 2;
   controls.maxDistance = 10;
-  controls.target.set( 0, 0, - 0.2 );
+  controls.target.set(0, 0, -0.2);
   controls.update();
 
-  window.addEventListener( 'resize', onWindowResize );
+  window.addEventListener('resize', onWindowResize);
 
 }
 
@@ -78,7 +100,7 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   render();
 
@@ -88,6 +110,6 @@ function onWindowResize() {
 
 function render() {
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 
 }
